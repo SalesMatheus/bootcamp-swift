edd9d1b8a7a1864c2afcebadaa43782b270c6c2c
Matheus Sales <matheus.carvalho.sales@gmail.com>
2022-08-01 07:46:19 -0300

    feat: add button roll

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,19 +13,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
 
+    
+    var arrayDiceNumber =  [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //diceImageView1.image = UIImage(named: "DiceSix")
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        
+        //diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
         //Snippet -> imageLiteral
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = #imageLiteral(resourceName: "DiceTwo")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFive")
+        
+        diceImageView1.image = arrayDiceNumber[Int.random(in: 0..<6)]
+        diceImageView2.image = arrayDiceNumber[Int.random(in: 0..<6)]
+
     }
     
 }
