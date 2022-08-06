452fa57b6c131eafa4cd4c83bd2442305bc70604
Matheus Sales <matheus.carvalho.sales@gmail.com>
2022-08-06 16:53:37 -0300

    teste

---
+++ projetos pequenos/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Matheus Sales on 30/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
