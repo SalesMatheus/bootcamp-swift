edea65486ee3133151ca1471e18b5f33e44377c2
Matheus Sales <matheus.carvalho.sales@gmail.com>
2022-08-06 15:28:46 -0300

    refactor: altered random numbers

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,21 +13,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
 
-    
-    var arrayDiceNumber =  [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        //diceImageView1.image = UIImage(named: "DiceSix")
-        //diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        //Snippet -> imageLiteral
-    }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
-        diceImageView1.image = arrayDiceNumber[Int.random(in: 0..<6)]
-        diceImageView2.image = arrayDiceNumber[Int.random(in: 0..<6)]
+        let diceArray  =  [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
 
     }
     
