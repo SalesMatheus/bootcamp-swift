c524a63fcd1031d4db2f58128e05863832cbc3c9
Matheus Sales <matheus.carvalho.sales@gmail.com>
2022-08-07 11:09:23 -0300

    feat: add ask ball

--- projetos pequenos/Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
+++ projetos pequenos/Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    @IBOutlet weak var magicBallView: UIImageView!
 
-
-
+    
+    @IBAction func asButtonPressed(_ sender: UIButton) {
+        let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+        
+        magicBallView.image = ballArray.randomElement()
+    }
+    
 }
 
