7504f77f04f706f97e44d8cef1beff9d57d257aa
Matheus Sales <matheus.carvalho.sales@gmail.com>
2022-08-23 08:14:19 -0300

    :sparkles: style: update layout

--- small-projects/Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
+++ small-projects/Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
@@ -13,12 +13,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
-    
+        
+    var stoy0 = "You see a fork in the road."
+    var choice1 = "Take a left."
+    var choice2 = "Take a right."
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        storyLabel.text = stoy0
+        choice1Button.setTitle(choice1, for: .normal)
+        choice2Button.setTitle(choice2, for: .normal)
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+    }
+    
 }
 
