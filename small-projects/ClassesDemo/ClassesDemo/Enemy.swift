26e4841f2e2e8e299564d7025be7984afa88b29b
Matheus Sales <matheus.carvalho.sales@gmail.com>
2022-08-27 11:00:41 -0300

    feat: game enemys class

--- small-projects/ClassesDemo/ClassesDemo/Enemy.swift
+++ small-projects/ClassesDemo/ClassesDemo/Enemy.swift
@@ -6,8 +6,17 @@
 //
 
 class Enemy {
-    var health = 100
-    var attackStrenght = 10
+    var health: Int
+    var attackStrenght: Int
+    
+    init(health: Int, attackStrenght: Int) {
+        self.health = health
+        self.attackStrenght = attackStrenght
+    }
+
+    func takeDamage(amount: Int) {
+        health = health - amount
+    }
     
     func move() {
         print("Walk forwards.")
--- small-projects/ClassesDemo/ClassesDemo/main.swift
+++ small-projects/ClassesDemo/ClassesDemo/main.swift
@@ -5,11 +5,11 @@
 //  Created by Matheus Sales on 26/08/22.
 //
 
-let skeleton = Enemy()
+let skeleton = Enemy(health: 100, attackStrenght: 10)
 
 
 
-let dragon = Dragon()
+let dragon = Dragon(health: 200, attackStrenght: 15)
 dragon.wingSpan = 5
 dragon.attackStrenght = 15
 dragon.talk(speech: "My teeth are swords! My claws are spears! My wings are a hurricane!")
