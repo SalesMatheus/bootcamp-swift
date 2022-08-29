3d4e387ffaf75e5cd8b222db153bcc6fe245b80f
Matheus Sales <matheus.carvalho.sales@gmail.com>
2022-08-29 08:20:46 -0300

    :recycle: refactor: refactor classes

---
+++ small-projects/BMI-Calculator-iOS13/BMI Calculator/Controllers/ResultsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ResultsViewController.swift
+//  BMI Calculator
+//
+//  Created by Matheus Sales on 29/08/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
