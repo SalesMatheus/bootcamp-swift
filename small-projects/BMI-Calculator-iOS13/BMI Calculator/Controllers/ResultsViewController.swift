6a386f97064352d11811c5d4fc2ff2f2e7dc7e30
Matheus Sales <matheus.carvalho.sales@gmail.com>
2022-08-29 08:57:10 -0300

    feat: send imc value to new screen

--- small-projects/BMI-Calculator-iOS13/BMI Calculator/Controllers/ResultsViewController.swift
+++ small-projects/BMI-Calculator-iOS13/BMI Calculator/Controllers/ResultsViewController.swift
@@ -10,26 +10,21 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
+    var bmiValue: String?
+
     @IBOutlet weak var bmiLabel: UILabel!
     @IBOutlet weak var adviceLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        bmiLabel.text = bmiValue
 
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
+        dismiss(animated: true)
     }
     
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
